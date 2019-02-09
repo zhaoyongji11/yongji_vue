3fb2502fb0a143cf1ae1d0662f626ea27eec92a6
zhaoyongji <yongj_03_04@163.com>
2019-02-09 11:23:16 +0800

    登陆完成

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Router from 'vue-router'
 import Login from '@/components/Login'
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -17,3 +17,14 @@ export default new Router({
 
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // 如果用户访问的登录页，直接放行
+  if (to.path === '/login') return next()
+  // 从 sessionStorage 中获取到 保存的 token 值
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 没有token，强制跳转到登录页
+  if (!tokenStr) return next('/login')
+  next()
+})
+export default router
